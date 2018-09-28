09bc26429a3cca830df568f7eef1361f23e1edd6
Leonardo Ledda <35930217+LeddaZ@users.noreply.github.com>
2018-09-28 12:18:22 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -30,10 +30,8 @@ const trigger26 = 'Punto Z';
 const trigger27 = 'punto z';
 const trigger28 = 'Paesaggio veneto';
 const trigger29 = 'paesaggio veneto';
-const trigger30 = 'Dispensa';
-const trigger31 = 'dispensa';
-const trigger32 = 'Farfalle';
-const trigger33 = 'farfalle';
+const trigger30 = 'Farfalle';
+const trigger31 = 'farfalle';
 const bot = new Bot(token, {polling: true});
 bot.on('message', (msg) => {
   if (msg.text.toString() === trigger)
@@ -53,11 +51,11 @@ bot.on('message', (msg) => {
   if (msg.text.toString() === trigger8)
     bot.sendMessage(msg.chat.id, 'MERJAAA VAI FUORI DALLA PORTAAAAAH!');
   if (msg.text.toString() === trigger9)
-    bot.sendMessage(msg.chat.id, 'Il Busi 1.1.2 by @LeddaZ - Digita BusiTrigger per la lista dei comandi - Codice del bot: https://github.com/LeddaZ/NastroAdesivoBot');
+    bot.sendMessage(msg.chat.id, 'Il Busi 1.1.3 by @LeddaZ - Digita BusiTrigger per la lista dei comandi - Codice del bot: https://github.com/LeddaZ/NastroAdesivoBot');
   if (msg.text.toString() === trigger10)
     bot.sendMessage(msg.chat.id, 'Il Busi 1.1.2 by @LeddaZ - Digita BusiTrigger per la lista dei comandi - Codice del bot: https://github.com/LeddaZ/NastroAdesivoBot');
   if (msg.text.toString() === trigger11)
-    bot.sendMessage(msg.chat.id, 'Comandi del Busi - Loddo; Non ho lo scotch; Buongiorno prof; Buongiorno; Businfo; Popopopo; Tigre; Merjaaa; Chi sei?; Chi sei tu?; Salute; Punto Z; Paesaggio veneto; Dispensa; Farfalle');
+    bot.sendMessage(msg.chat.id, 'Comandi del Busi - Loddo; Non ho lo scotch; Buongiorno prof; Buongiorno; Businfo; Popopopo; Tigre; Merjaaa; Chi sei?; Chi sei tu?; Salute; Punto Z; Paesaggio veneto; Farfalle');
   if (msg.text.toString() === trigger12)
     bot.sendMessage(msg.chat.id, 'CHI È QUESTO? AH È GUERAAAAAAH!');
   if (msg.text.toString() === trigger13)
@@ -95,11 +93,7 @@ bot.on('message', (msg) => {
   if (msg.text.toString() === trigger29)
     bot.sendMessage(msg.chat.id, "http://www.paesaggioveneto.it/");
   if (msg.text.toString() === trigger30)
-    bot.sendMessage(msg.chat.id, "http://pdfitalia.link/arte-cinema-e-fotografia/dispensa-di-disegno-tecnico.pdf");
-  if (msg.text.toString() === trigger31)
-    bot.sendMessage(msg.chat.id, "http://pdfitalia.link/arte-cinema-e-fotografia/dispensa-di-disegno-tecnico.pdf");
-  if (msg.text.toString() === trigger32)
     bot.sendMessage(msg.chat.id, "Le farfalle fanno AAAAAHHHHHH!");
-  if (msg.text.toString() === trigger33)
+  if (msg.text.toString() === trigger31)
     bot.sendMessage(msg.chat.id, "Le farfalle fanno AAAAAHHHHHH!");
 })
