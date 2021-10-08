765822bf00dc5c9fe34e8f3e39bfc2c777379e0f
LeddaZ <leonardoledda@gmail.com>
2021-10-08 21:35:52 +0200

    Semplificazione della data

--- renato.js
+++ renato.js
@@ -47,13 +47,8 @@ modificato per l'ultima volta)
 const stats = fs.statSync("package.json");
 var mtime = stats.mtime;
 
-// Formato della data (g/m/a)
-var g = { day: 'numeric' };
-var m = { month: 'numeric' };
-var a = { year: 'numeric' };
-
-// Creazione della stringa con la data
-var data = mtime.toLocaleDateString('it-IT', g) + "/" + mtime.toLocaleDateString('it-IT', m) + "/" + mtime.toLocaleDateString('it-IT', a);
+// Creazione della stringa con la data (g/m/a)
+var data = mtime.toLocaleDateString('it-IT');
 
 // Testo di /businfo e /start
 var start = "<b>NastroAdesivoBot</b>\nVersione <code>" + ver + "</code> del " + data + "\nDigita /busitrigger per la lista di trigger e comandi\n<a href=\"https://github.com/LeddaZ/NastroAdesivoBot/\">Codice sorgente</a> - <a href=\"https://github.com/LeddaZ/NastroAdesivoBot/blob/master/extra/changelog.md\">Changelog</a>\nIspirato al mitico <b>Renato Busata</b> e creato da @LeddaZ"
